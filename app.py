8b53638c83e6f5bed9dce15d75bcbedc465f0f14
kailas nath A <vijayasre284@gmail.com>
2025-06-12 14:39:12 +0530

    Fix: updated app.run to work on Render

--- app.py
+++ app.py
@@ -18,4 +18,7 @@ def predict():
     return render_template('index.html', prediction_text=f'Predicted Price: ${{prediction[0]:.2f}}')
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    import os
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port, debug=True)
+
