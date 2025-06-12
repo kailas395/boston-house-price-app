a0d0c90eef8275c9cd1da03684988d8ea19a314a
kailas nath A <vijayasre284@gmail.com>
2025-06-12 14:56:08 +0530

    Fix: fetch form inputs feature0 to feature12 in order

--- app.py
+++ app.py
@@ -1,4 +1,3 @@
-
 from flask import Flask, render_template, request
 import numpy as np
 import joblib
@@ -12,13 +11,18 @@ def home():
 
 @app.route('/predict', methods=['POST'])
 def predict():
-    input_data = [float(x) for x in request.form.values()]
-    input_array = np.array([input_data])
-    prediction = model.predict(input_array)
-    return render_template('index.html', prediction_text=f'Predicted Price: ${{prediction[0]:.2f}}')
+    try:
+        # Extract input values in order from feature0 to feature12
+        input_data = [float(request.form[f'feature{i}']) for i in range(13)]
+        input_array = np.array([input_data])
+        prediction = model.predict(input_array)
+        return render_template('index.html', prediction_text=f'Predicted Price: ${prediction[0]:.2f}')
+    except Exception as e:
+        return render_template('index.html', prediction_text=f'Error: {str(e)}')
 
 if __name__ == '__main__':
     import os
     port = int(os.environ.get("PORT", 5000))
     app.run(host='0.0.0.0', port=port, debug=True)
+
 
